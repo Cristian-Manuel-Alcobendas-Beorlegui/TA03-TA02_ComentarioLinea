45fcc26cfd3599047546a0301f181e0785c7cd11
usuario <usuario@LAPTOP-IR96S2FN>
2022-09-07 17:44:44 +0200

    Repositorio para el proyecto "TA03 - Comentario en una línea"

---
+++ TA03-ComentarioLinea/src/TA03/ComentarioLinea.java
@@ -0,0 +1,9 @@
+package TA03;
+
+public class ComentarioLinea{
+	public static void main(String[] args) {
+		// System.out.println("Hola");
+		System.out.println("Adiós");
+		System.out.println("FIN DE PROGRAMA");
+	}
+}
